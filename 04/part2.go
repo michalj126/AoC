f5aad026335887d38762ee94a5d985fd9feec64a
Michał Janoszczyk <michal.janoszczyk@gmail.com>
2022-12-04 19:20:43 +0100

    (day 4): add solution

---
+++ 04/part2.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Pair struct {
+	begin int
+	end   int
+}
+
+func main() {
+	path := os.Args[1]
+
+	f, err := os.Open(path)
+	check(err)
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	sum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		firstPair, secondPair := splitPairs(line)
+
+		if isOverlap(firstPair, secondPair) {
+			sum++
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func splitPairs(input string) (Pair, Pair) {
+	splittedPairs := strings.Split(input, ",")
+
+	splittedFirstPair := strings.Split(splittedPairs[0], "-")
+	splittedSecondPair := strings.Split(splittedPairs[1], "-")
+
+	firstPair := Pair{
+		begin: str2Int(splittedFirstPair[0]),
+		end:   str2Int(splittedFirstPair[1]),
+	}
+
+	secondPair := Pair{
+		begin: str2Int(splittedSecondPair[0]),
+		end:   str2Int(splittedSecondPair[1]),
+	}
+
+	return firstPair, secondPair
+}
+
+func str2Int(input string) int {
+	i, err := strconv.Atoi(input)
+	check(err)
+
+	return i
+}
+
+func isOverlap(x Pair, y Pair) bool {
+	if y.begin > x.end || y.end < x.begin {
+		return false
+	}
+
+	return true
+}
