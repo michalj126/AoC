ed12b4310ee6b3a2f5eeaa00982da860f7eb37af
Michał Janoszczyk <michal.janoszczyk@gmail.com>
2022-12-06 12:14:57 +0100

    (day 6): add solution

---
+++ 06/part1.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Stack struct {
+	crates []string
+}
+
+func main() {
+	path := os.Args[1]
+
+	f, err := os.Open(path)
+	check(err)
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	datastream := ""
+
+	for scanner.Scan() {
+		datastream = scanner.Text()
+	}
+
+	_, index := findMarker(datastream)
+
+	fmt.Println(index)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func findMarker(datastream string) (bool, int) {
+	datastreamLength := len(datastream)
+
+	chunkSize := 4
+
+	for i := range datastream {
+		if i+chunkSize <= datastreamLength {
+			chunk := datastream[i : i+chunkSize]
+
+			if isMarker(chunk) {
+				return true, strings.Index(datastream, chunk) + chunkSize
+			}
+		}
+	}
+
+	return false, -1
+}
+
+func isMarker(chunk string) bool {
+	result := true
+
+	for _, v := range chunk {
+		if strings.Count(chunk, string(v)) > 1 {
+			result = false
+		}
+	}
+
+	return result
+}
